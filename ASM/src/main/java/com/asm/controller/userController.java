035a7a4701e711fc54f4c1f2139da0ad313f8a8a
trong12344 <trongmap2k3>
2023-06-12 10:10:28 +0700

    asd

--- ASM/src/main/java/com/asm/controller/userController.java
+++ ASM/src/main/java/com/asm/controller/userController.java
@@ -3,10 +3,14 @@ package com.asm.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.asm.interfaces.UserInterfaces;
 import com.asm.model.Account;
+import com.asm.model.Category;
+import com.asm.model.Product;
 @Controller
 public class userController {
 	@Autowired
@@ -16,4 +20,17 @@ public class userController {
 		model.addAttribute("user", udao.findAll());
 		return "QuanliUser";
 	}
+	@RequestMapping("/edit-user/{id}")
+	public String edit(@PathVariable("id") String id, Model model, @ModelAttribute("user") Account account) {
+		Account user = udao.findById(id).get();		
+		account.setFullname(user.getFullname());
+		account.setUsername(user.getUsername());
+		account.setPassword(user.getPassword());
+		account.setEmail(user.getEmail());
+		model.addAttribute("account", account);
+		return "editUser";
+		
+	}
+	
+	
 }
